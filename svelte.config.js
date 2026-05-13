b9b17c94be575e4a0d38576da88e012e53d46cfe
LeFrei <frei.leonie@johak.at>
2026-05-13 11:42:02 +0200

    Change base path to /blackjack/page

--- svelte.config.js
+++ svelte.config.js
@@ -11,7 +11,7 @@ const config = {
 			fallback: '404.html'
 		}),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/blackjack' : ''
+			base: process.env.NODE_ENV === 'production' ? '/blackjack/page' : ''
 		}
 	}
 };
